5a860d22ddd8e26728cbeed9b31f33ffd5a20980
Antoine Magnin <ant.magnin@gmail.com>
2014-11-25 15:37:45 +0100

    ~ Class Torrent added Constructor implemented

---
+++ ProjetBitTorrent/src/Torrent.java
@@ -0,0 +1,56 @@
+import java.security.MessageDigest;
+import java.io.RandomAccessFile;
+
+public class Torrent {
+
+	private Metafile torrent = null;
+	private String infoHash = null;
+	private String peerID = null;
+	private Integer uploaded = null;
+	private Integer downloaded = null;
+	private Integer left = null;
+	private String ip = null;
+	private Integer numWant = null;
+	private String event = null;
+	
+	public Torrent(Metafile file) {
+		
+		// Get the torrent file
+		this.torrent = file;
+		
+		// Allocate memory on disk for downloaded file
+		try {
+			RandomAccessFile tmp = new RandomAccessFile(torrent.getName(), "rw");
+			tmp.setLength(1024 * 1024);
+			tmp.close();
+		} catch (Exception e) {
+			System.err.println("Error while creating tmp file");	
+		}
+		
+		// Get SHA-1 infoHash
+		try {
+			MessageDigest md = null;
+			md = MessageDigest.getInstance("SHA-1");
+			byte[] sha1 = md.digest("lol".getBytes());
+			this.infoHash = URLEncoder.encode(sha1.toString(), "UTF-8");
+		} catch (Exception e) {
+			System.err.println("Error while getting SHA-1 infoHash");
+		}
+		
+		// Set downloaded, uploaded and left values
+		this.uploaded = 0;
+		this.downloaded = 0;
+		this.left = this.torrent.getLength();
+		
+		// Set event status
+		this.event = "started";
+		
+	}
+	
+	public String Request(SortedMap params) {
+		
+		
+		
+	}
+
+}
