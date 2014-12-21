fbe51dd3b6c97de4c4bd781bf3cad6348d3ae966
Antoine Magnin <Antoine@MacBook-Pro-de-Antoine.local>
2014-12-21 01:46:00 +0100

    Implementation du protocole PWP : - La classe PeerConnection devient un thread - Ajout d'une sous classe ProcessMessage pour le traitement des messages - Ajout d'une sous classe ReceiveMessage pour l'écoute des messages - Ajout d'une sous classe SendMessage pour l'envoi des messages - Chaque sous-classe est un thread

--- ProjetBitTorrent/src/PeerConnection.java
+++ ProjetBitTorrent/src/PeerConnection.java
@@ -7,8 +7,10 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Arrays;
+import java.nio.ByteBuffer;
 
-public class PeerConnection {
+public class PeerConnection extends Thread {
+	
 	final int TIME_OUT_CONNECTION = 5000;
 	final int TIME_OUT_HANDSHAKE = 5000;
 
@@ -17,22 +19,31 @@ public class PeerConnection {
 	String peerID;
 	byte[] pieces;
 	Peer peer;
-
 	Socket socket;
+	Boolean handshaken = false;
+	Boolean choked = false;
+	Boolean interested = false;
 
 	public PeerConnection(Peer peer, byte[] pieces, int[] piecesDownloaded, byte[] infoHash, String peerID) throws IOException {
+		
+		// Initialize attributes
 		this.peer = peer;
 		this.piecesDownloaded = piecesDownloaded;
 		this.pieces = pieces;
 		this.infoHash = infoHash;
 		this.peerID = peerID;
 
+		// Initlialize connection to peer
 		System.out.print("=========================================================" + "\n");
 		System.out.println("Connection to " + peer.toString());
 		System.out.flush();
 		socket = new Socket();
 		socket.connect(new InetSocketAddress(peer.getIP(), peer.getPort()), TIME_OUT_CONNECTION);
 		System.out.println("Connected to peer " + peer.toString());
+		
+		// Proceed to the handshake
+		handshaken = handshake();
+		
 	}
 
 	public Boolean handshake() {
@@ -52,6 +63,7 @@ public class PeerConnection {
 		DataOutputStream mDataOut;
 
 		try {
+			
 			// Retrieve all socket streams
 			mOut = socket.getOutputStream();
 			mIn = socket.getInputStream();
@@ -61,6 +73,7 @@ public class PeerConnection {
 			// Set time out value
 			socket.setSoTimeout(TIME_OUT_HANDSHAKE);
 			System.out.println("sending handshake");
+			
 			// Send handshake
 			mDataOut.write(handshake);
 			mDataOut.flush();
@@ -74,22 +87,30 @@ public class PeerConnection {
 			mDataIn.readFully(response);
 
 			System.out.println("Response received");
-			System.out.println(response.length);
 
 			byte[] responseInfoHash = Arrays.copyOfRange(response, 28, 48);
 			byte sizeProtocolName = response[0];
 
 			// Make sure that is the bittorrent protocol
 			if ((sizeProtocolName == 19 ) && new String(response).toLowerCase().contains("bittorrent protocol")) {
+				
 				// If the hash send is the same as the hash received
 				if (Arrays.equals(infoHash, responseInfoHash)) {
+					
 					// Extract peer ID and store it
 					String peerID = new String(Arrays.copyOfRange(response, 48, 68));
 					peer.setPeerID(peerID);
-					return true; // Handshake is correct.
+					
+					// Handshake is correct.
+					return true;
+					
 				}
+				
 			}
-			return false; // Handshake is incorrect
+			
+			// Handshake is incorrect
+			return false; 
+			
 		} catch (EOFException e) {
 			System.out.print("Data from " + peer.toString() + " are corrupted." + "\n");
 			return false;
@@ -97,6 +118,98 @@ public class PeerConnection {
 			System.out.print("Error while tryin to read data input stream from " + peer.toString() + "\n");
 			return false;
 		}
+		
 	}
+	
+	public void run() {
+		
+		// Start listening to peer
+		ReceiveMessages rm = new ReceiveMessages();
+		rm.start();
+		
+	}
+		
+	public class ProcessMessage extends Thread {
+		
+		byte[] msg;
+		
+		public ProcessMessage(byte[] msg) {
+			this.msg = msg;
+		}
+		
+		public void run() {
+			
+			System.out.println("New message with ID : " + msg[4]);
+
+		}
+		
+	}
+	
+	public class ReceiveMessages extends Thread {
+
+		public void run() {
+			
+			try {
+
+				ByteBuffer bb = ByteBuffer.allocate(1500);
+				int msgLength = 1500;
+				DataInputStream input = new DataInputStream(socket.getInputStream());
+				System.out.println("Start listening to messages");
+				
+				while (true) {
+
+					// Check if some data is available
+					int size = input.available();
+					
+					// Some data is available
+					if (size > 0) {
+						
+						// Get bytes on by one
+						for (int i = 0; i < size; i++) {
+							
+							bb.put(input.readByte());
+							
+							// Get the length of the message
+							if (bb.position() == 4) {
+								msgLength = bb.getInt(0);
+								System.out.println("Message length : " + bb.getInt(0));
+							}
+							
+							// Avoid "keep-alive" messages
+							if (msgLength == 0) {
+								bb.clear();	
+							}
+							
+							// Get and process the message
+							if (bb.position() == msgLength + 4) {
+								byte[] msg = new byte[msgLength + 4];
+								bb.rewind();
+								bb.get(msg, 0, msgLength + 4);
+								bb.clear();
+								ProcessMessage pm = new ProcessMessage(msg);
+								pm.start();
+							}
+							
+						}
+						
+					}
+					
+				}
+				
+			} catch (Exception e) {
+				System.err.println("Error while reading socket");
+			}
+
+		}
+
+	}
+	
+	public class SendMessage extends Thread {
+		
+		public void run() {
+			
+		}
+		
+	}	
 
 }
--- ProjetBitTorrent/src/PeersManager.java
+++ ProjetBitTorrent/src/PeersManager.java
@@ -3,12 +3,12 @@ import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 
 public class PeersManager {
+	
 	Peers peers;
 	byte[] pieces;
 	int[] piecesDownloaded; // 0 = non downloaded, 1 = in progress , 2 = download finished
 	byte[] infoHash;
 	String peerID;
-
 	ArrayList<PeerConnection> peerConnections;
 
 	public PeersManager(Peers peers, byte[] pieces, byte[] infoHash, String peerID) {
@@ -19,28 +19,40 @@ public class PeersManager {
 		this.peerID = peerID;
 
 		piecesDownloaded = new int[pieces.length / 20];
+		
 		// All pieces set to non downloaded
 		for (int i = 0; i < piecesDownloaded.length; i++) {
 			piecesDownloaded[i] = 0;
 		}
+		
 		peerConnections = new ArrayList<PeerConnection>();
 
 	}
 
 	public void startDownload() {
+		
 		ArrayList<Peer> listPeers = peers.getPeers();
 
 		// Do all handhsakes
 		for (Peer peer : listPeers) {
+			
 			try {
+				
 				// Throw exception if connection drop out
 				PeerConnection peerConnection = new PeerConnection(peer, pieces, piecesDownloaded, infoHash, peerID);
+				
 				// If the handshake worked as expected, return true.
-				if(peerConnection.handshake()){
+				if (peerConnection.handshaken) {
+					
 					// At this point we know that peerConnection is valid because the connection worked and the handshake too
 					this.peerConnections.add(peerConnection);
 					System.out.println("Handshake OK");
+					
+					peerConnection.start();
+					break;
+					
 				} // else, discard connection
+				
 			} catch (SocketTimeoutException e) {
 				System.out.print("Time out connection to " + peer.toString() + "." + "\n");
 			} catch (ConnectException e) {
@@ -48,6 +60,9 @@ public class PeersManager {
 			} catch (Exception e) {
 				System.out.print("Unexpected error while trying to connect to " + peer.toString() + "\n");
 			}
+			
 		}
+		
 	}
+	
 }
